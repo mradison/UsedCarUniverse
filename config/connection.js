8dda129def9c72c3bb426939ab8b4b97ae705bdd
madison reynolds <madison.reynolds@phoenix.edu>
2023-12-04 19:33:29 -0700

    trying to fix the connection.js file so it will work on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,4 @@
-var sequelize;
-
-// require('dotenv').config();
+let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
@@ -17,15 +15,34 @@ if (process.env.JAWSDB_URL) {
   );
 }
 
-let sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+// var sequelize;
 
-module.exports = sequelize;
+// // require('dotenv').config();
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PW,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306,
+//     },
+//   );
+// }
+
+// let sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306,
+//   }
+// );
+
+// module.exports = sequelize;
